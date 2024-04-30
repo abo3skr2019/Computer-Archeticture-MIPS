846ac1a9f06690b0434f1474e2b4de3b598f1e84
AF_Askar <68832286+abo3skr2019@users.noreply.github.com>
2024-04-30 18:50:49 +0300

    Add code to print A[B[2]] and a new line in Assignment1Q1.asm

--- Assignment1Q1.asm
+++ Assignment1Q1.asm
@@ -13,7 +13,7 @@ main:
     sll $t1, $t1, 2             # $t1 = $t1 * 4 (byte offset)
     add $t1, $a1, $t1           # $t1 = base address of B + byte offset
     lw $t0, 0($t1)              # $t0 = B[2]
-
+    
     # Check if B[2] is greater than or equal to 0
     bgez $t0, compute           # if B[2] >= 0, jump to compute
 
@@ -25,6 +25,16 @@ main:
     sll $t0, $t0, 2             # $t0 = B[2] * 4 (byte offset)
     add $t0, $a0, $t0           # $t0 = base address of A + byte offset
     lw $t2, 0($t0)              # $t2 = A[B[2]]
+
+
+    # Print A[B[2]]
+    move $a0, $t2  # Move the value of $t2 to $a0
+    li $v0, 1      # Load the system call code for print_int into $v0
+    syscall        # Make the system call
+    # Print New Line
+    li $a0, 10     # Load ASCII value of newline into $a0
+    li $v0, 11     # Load the system call code for print_char into $v0
+    syscall        # Make the system call
 
     # Compute A[B[2]] * 2 and store in X ($s0)
     sll $s0, $t2, 1             # $s0 = A[B[2]] * 2
